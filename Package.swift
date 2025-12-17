06d3713dc8108398356dcfd3a53fe136b59b1446
signimarc <marc.tamarit@signicat.com>
2025-12-17 22:21:41 +0100

    Upgrade to version 1.49.0-RC

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let version: String = "1.49.0-RC"
 let urlVersion: String = "1.49.0"
-let checksum: String = "e87e50f02a1e38035d16131066204bd077dd9825c4033fe5d37efe0f7dc8508d"
+let checksum: String = "e7c2785439c19b50d56da6f88b43bb52150551134facf61ee608e7224f1fcc2b"
 
 
 let package = Package(
